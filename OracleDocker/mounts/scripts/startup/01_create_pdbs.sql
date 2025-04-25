ab763860c958b02495f95614aec23d354ca3c00a
Bina Mircea <67867765+mirceaiscoding@users.noreply.github.com>
2025-04-25 23:18:47 +0300

    Create a singular container for all the databases.

--- OracleDocker/mounts/scripts/startup/01_create_pdbs.sql
+++ OracleDocker/mounts/scripts/startup/01_create_pdbs.sql
@@ -3,53 +3,60 @@ SET SERVEROUTPUT ON;
 -- Switch to the root container
 ALTER SESSION SET CONTAINER = CDB$ROOT;
 
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
 
--- Check if PDB eshop_oltp exists
+-- Create PDBs
 DECLARE
     pdb_exists NUMBER := 0;
 BEGIN
-    SELECT COUNT(*)
-    INTO pdb_exists
-    FROM dba_pdbs
-    WHERE pdb_name = 'ESHOP_OLTP';
-
+    -- Create ESHOP_OLTP PDB
+    SELECT COUNT(*) INTO pdb_exists FROM V$PDBS WHERE NAME = 'ESHOP_OLTP';
     IF pdb_exists = 0 THEN
-        -- Create PDB for OLTP
-        EXECUTE IMMEDIATE '
-            CREATE PLUGGABLE DATABASE eshop_oltp 
-            ADMIN USER eshop_oltp_admin IDENTIFIED BY oltp_password 
-            ROLES = (DBA) 
-            FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed/'', ''/opt/oracle/oradata/ORCLCDB/eshop_oltp/'')
-        ';
-        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE eshop_oltp OPEN';
-        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB eshop_oltp was successfully created and opened.');
+        EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_OLTP ADMIN USER eshop_oltp_admin IDENTIFIED BY "oltp_password" ROLES = (DBA) FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/ORCLCDB/ESHOP_OLTP'')';
+        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_OLTP OPEN';
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_OLTP was successfully created and opened.');
     ELSE
-        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB eshop_oltp already exists.');
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_OLTP already exists.');
     END IF;
-END;
-/
 
--- Check if PDB eshop_dw exists
-DECLARE
-    pdb_exists NUMBER := 0;
-BEGIN
-    SELECT COUNT(*)
-    INTO pdb_exists
-    FROM dba_pdbs
-    WHERE pdb_name = 'ESHOP_DW';
-
+    -- Create ESHOP_DW PDB
+    SELECT COUNT(*) INTO pdb_exists FROM V$PDBS WHERE NAME = 'ESHOP_DW';
     IF pdb_exists = 0 THEN
-        -- Create PDB for Data Warehouse
-        EXECUTE IMMEDIATE '
-            CREATE PLUGGABLE DATABASE eshop_dw 
-            ADMIN USER eshop_dw_admin IDENTIFIED BY dw_password 
-            ROLES = (DBA) 
-            FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed/'', ''/opt/oracle/oradata/ORCLCDB/eshop_dw/'') 
-        ';
-        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE eshop_dw OPEN';
-        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB eshop_dw was successfully created and opened.');
+        EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_DW ADMIN USER eshop_dw_admin IDENTIFIED BY "dw_password" ROLES = (DBA) FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/ORCLCDB/ESHOP_DW'')';
+        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_DW OPEN';
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_DW was successfully created and opened.');
     ELSE
-        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB eshop_dw already exists.');
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_DW already exists.');
+    END IF;
+
+    -- Create ESHOP_GLOBAL PDB
+    SELECT COUNT(*) INTO pdb_exists FROM V$PDBS WHERE NAME = 'ESHOP_GLOBAL';
+    IF pdb_exists = 0 THEN
+        EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_GLOBAL ADMIN USER global_admin IDENTIFIED BY "GlobalAdminPassword123!" FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/GLOBALCDB/ESHOP_GLOBAL'')';
+        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_GLOBAL OPEN';
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_GLOBAL was successfully created and opened.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_GLOBAL already exists.');
+    END IF;
+
+    -- Create ESHOP_MUNTENIA PDB
+    SELECT COUNT(*) INTO pdb_exists FROM V$PDBS WHERE NAME = 'ESHOP_MUNTENIA';
+    IF pdb_exists = 0 THEN
+        EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_MUNTENIA ADMIN USER muntenia_admin IDENTIFIED BY "MunteniaAdminPassword123!" FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/MUNTENIACDB/ESHOP_MUNTENIA'')';
+        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_MUNTENIA OPEN';
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_MUNTENIA was successfully created and opened.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_MUNTENIA already exists.');
+    END IF;
+
+    -- Create ESHOP_ROMANIA PDB
+    SELECT COUNT(*) INTO pdb_exists FROM V$PDBS WHERE NAME = 'ESHOP_ROMANIA';
+    IF pdb_exists = 0 THEN
+        EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_ROMANIA ADMIN USER romania_admin IDENTIFIED BY "RomaniaAdminPassword123!" FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/ROMANIACDB/ESHOP_ROMANIA'')';
+        EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_ROMANIA OPEN';
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_ROMANIA was successfully created and opened.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('create_pdbs: PDB ESHOP_ROMANIA already exists.');
     END IF;
 END;
 /
