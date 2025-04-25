61a16f2492aa4ac7083f08132c4c049920a9b49c
NacuAndrei <nacuandrei10@gmail.com>
2025-04-25 13:01:25 +0300

    Fix admin login for eshop_global

--- OracleDocker/mounts/scripts/startup_global/01_global_create_pdb.sql
+++ OracleDocker/mounts/scripts/startup_global/01_global_create_pdb.sql
@@ -10,6 +10,36 @@ BEGIN
     -- Create the ESHOP_GLOBAL PDB
     EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_GLOBAL ADMIN USER global_admin IDENTIFIED BY "GlobalAdminPassword123!" FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/GLOBALCDB/ESHOP_GLOBAL'')';
     EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_GLOBAL OPEN';
+    DBMS_OUTPUT.PUT_LINE('ESHOP_GLOBAL PDB was successfully created and opened.');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('ESHOP_GLOBAL PDB already exists.');
   END IF;
+END;
+/
+
+-- Ensure the GLOBAL_ADMIN user exists and has the DBA role
+BEGIN
+  EXECUTE IMMEDIATE 'ALTER SESSION SET CONTAINER = ESHOP_GLOBAL';
+
+  -- Check if the GLOBAL_ADMIN user exists
+  DECLARE
+    user_exists NUMBER := 0;
+  BEGIN
+    SELECT COUNT(*) INTO user_exists FROM DBA_USERS WHERE USERNAME = 'GLOBAL_ADMIN';
+
+    IF user_exists = 0 THEN
+      -- Create the GLOBAL_ADMIN user
+      EXECUTE IMMEDIATE 'CREATE USER global_admin IDENTIFIED BY "GlobalAdminPassword123!"';
+      DBMS_OUTPUT.PUT_LINE('GLOBAL_ADMIN user was successfully created.');
+    ELSE
+      -- Update the password for GLOBAL_ADMIN if it already exists
+      EXECUTE IMMEDIATE 'ALTER USER global_admin IDENTIFIED BY "GlobalAdminPassword123!"';
+      DBMS_OUTPUT.PUT_LINE('GLOBAL_ADMIN user password was updated.');
+    END IF;
+
+    -- Grant the DBA role to GLOBAL_ADMIN
+    EXECUTE IMMEDIATE 'GRANT DBA TO global_admin';
+    DBMS_OUTPUT.PUT_LINE('GLOBAL_ADMIN user was granted the DBA role.');
+  END;
 END;
 /
