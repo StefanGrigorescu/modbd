46c197be3b8617a6d8de0c561c7684e13df2f226
NacuAndrei <nacuandrei10@gmail.com>
2025-04-25 13:10:50 +0300

    Fix admin for muntenia & romania

--- OracleDocker/mounts/scripts/startup_muntenia/01_muntenia_create_pdb.sql
+++ OracleDocker/mounts/scripts/startup_muntenia/01_muntenia_create_pdb.sql
@@ -10,6 +10,36 @@ BEGIN
     -- Create the ESHOP_MUNTENIA PDB
     EXECUTE IMMEDIATE 'CREATE PLUGGABLE DATABASE ESHOP_MUNTENIA ADMIN USER muntenia_admin IDENTIFIED BY "MunteniaAdminPassword123!" FILE_NAME_CONVERT = (''/opt/oracle/oradata/ORCLCDB/pdbseed'', ''/opt/oracle/oradata/MUNTENIACDB/ESHOP_MUNTENIA'')';
     EXECUTE IMMEDIATE 'ALTER PLUGGABLE DATABASE ESHOP_MUNTENIA OPEN';
+    DBMS_OUTPUT.PUT_LINE('ESHOP_MUNTENIA PDB was successfully created and opened.');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('ESHOP_MUNTENIA PDB already exists.');
   END IF;
+END;
+/
+
+-- Ensure the MUNTENIA_ADMIN user exists and has the DBA role
+BEGIN
+  EXECUTE IMMEDIATE 'ALTER SESSION SET CONTAINER = ESHOP_MUNTENIA';
+
+  -- Check if the MUNTENIA_ADMIN user exists
+  DECLARE
+    user_exists NUMBER := 0;
+  BEGIN
+    SELECT COUNT(*) INTO user_exists FROM DBA_USERS WHERE USERNAME = 'MUNTENIA_ADMIN';
+
+    IF user_exists = 0 THEN
+      -- Create the MUNTENIA_ADMIN user
+      EXECUTE IMMEDIATE 'CREATE USER muntenia_admin IDENTIFIED BY "MunteniaAdminPassword123!"';
+      DBMS_OUTPUT.PUT_LINE('MUNTENIA_ADMIN user was successfully created.');
+    ELSE
+      -- Update the password for MUNTENIA_ADMIN if it already exists
+      EXECUTE IMMEDIATE 'ALTER USER muntenia_admin IDENTIFIED BY "MunteniaAdminPassword123!"';
+      DBMS_OUTPUT.PUT_LINE('MUNTENIA_ADMIN user password was updated.');
+    END IF;
+
+    -- Grant the DBA role to MUNTENIA_ADMIN
+    EXECUTE IMMEDIATE 'GRANT DBA TO muntenia_admin';
+    DBMS_OUTPUT.PUT_LINE('MUNTENIA_ADMIN user was granted the DBA role.');
+  END;
 END;
 /
