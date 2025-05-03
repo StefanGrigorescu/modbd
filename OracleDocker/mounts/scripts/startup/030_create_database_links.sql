a40ae468607db6378119e326f5cc4773ad03564a
Bina Mircea <67867765+mirceaiscoding@users.noreply.github.com>
2025-05-03 22:53:31 +0300

    try to create the links correctly

--- OracleDocker/mounts/scripts/startup/030_create_database_links.sql
+++ OracleDocker/mounts/scripts/startup/030_create_database_links.sql
@@ -1,82 +1,139 @@
+-- Enable DBMS_OUTPUT
+SET SERVEROUTPUT ON;
+
 -- Switch to ESHOP_GLOBAL PDB
 ALTER SESSION SET CONTAINER = eshop_global;
 
-DECLARE
-    link_exists NUMBER := 0;
-BEGIN
-    SELECT COUNT(*) INTO link_exists
-    FROM ALL_DB_LINKS
-    WHERE DB_LINK = 'LINK_TO_MUNTENIA';
+-- Set the schema to the desired user
+ALTER SESSION SET CURRENT_SCHEMA = ESHOP_GLOBAL_USER;
 
-    IF link_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE DATABASE LINK link_to_muntenia
-            CONNECT TO "muntenia_admin" IDENTIFIED BY "MunteniaAdminPassword123!"
-            USING ''(DESCRIPTION =
+-- Drop and recreate LINK_TO_MUNTENIA
+BEGIN
+    EXECUTE IMMEDIATE 'DROP DATABASE LINK link_to_muntenia';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN -- Ignore "link does not exist" error
+            RAISE;
+        END IF;
+END;
+/
+
+EXECUTE IMMEDIATE '
+    CREATE DATABASE LINK link_to_muntenia
+    CONNECT TO eshop_muntenia_user IDENTIFIED BY ''MunteniaUserPassword123!''
+    USING ''(DESCRIPTION =
                 (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
                 (CONNECT_DATA = (SERVICE_NAME = ESHOP_MUNTENIA)))''';
-    END IF;
+/
+
+-- Test LINK_TO_MUNTENIA
+BEGIN
+    FOR rec IN (SELECT 'LINK_TO_MUNTENIA is working' AS status FROM dual@link_to_muntenia) LOOP
+        DBMS_OUTPUT.PUT_LINE(rec.status);
+    END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error testing LINK_TO_MUNTENIA: ' || SQLERRM);
 END;
 /
 
-DECLARE
-    link_exists NUMBER := 0;
+-- Drop and recreate LINK_TO_ROMANIA
 BEGIN
-    SELECT COUNT(*) INTO link_exists
-    FROM ALL_DB_LINKS
-    WHERE DB_LINK = 'LINK_TO_ROMANIA';
+    EXECUTE IMMEDIATE 'DROP DATABASE LINK link_to_romania';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN -- Ignore "link does not exist" error
+            RAISE;
+        END IF;
+END;
+/
 
-    IF link_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE DATABASE LINK link_to_romania
-            CONNECT TO "romania_admin" IDENTIFIED BY "RomaniaAdminPassword123!"
-            USING ''(DESCRIPTION =
+EXECUTE IMMEDIATE '
+    CREATE DATABASE LINK link_to_romania
+    CONNECT TO eshop_romania_user IDENTIFIED BY ''RomaniaUserPassword123!''
+    USING ''(DESCRIPTION =
                 (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
                 (CONNECT_DATA = (SERVICE_NAME = ESHOP_ROMANIA)))''';
-    END IF;
-END;
 /
 
+-- Test LINK_TO_ROMANIA
+BEGIN
+    FOR rec IN (SELECT 'LINK_TO_ROMANIA is working' AS status FROM dual@link_to_romania) LOOP
+        DBMS_OUTPUT.PUT_LINE(rec.status);
+    END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error testing LINK_TO_ROMANIA: ' || SQLERRM);
+END;
+/
 
 -- Switch to ESHOP_MUNTENIA PDB
 ALTER SESSION SET CONTAINER = eshop_muntenia;
 
-DECLARE
-    link_exists NUMBER := 0;
-BEGIN
-    SELECT COUNT(*) INTO link_exists
-    FROM ALL_DB_LINKS
-    WHERE DB_LINK = 'LINK_TO_GLOBAL';
+-- Set the schema to the desired user
+ALTER SESSION SET CURRENT_SCHEMA = ESHOP_MUNTENIA_USER;
 
-    IF link_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE DATABASE LINK link_to_global
-            CONNECT TO "global_admin" IDENTIFIED BY "GlobalAdminPassword123!"
-            USING ''(DESCRIPTION =
-                (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
-                (CONNECT_DATA = (SERVICE_NAME = ESHOP_GLOBAL)))''';
-    END IF;
+-- Drop and recreate LINK_TO_GLOBAL
+BEGIN
+    EXECUTE IMMEDIATE 'DROP DATABASE LINK link_to_global';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN -- Ignore "link does not exist" error
+            RAISE;
+        END IF;
 END;
 /
 
+EXECUTE IMMEDIATE '
+    CREATE DATABASE LINK link_to_global
+    CONNECT TO eshop_global_user IDENTIFIED BY ''GlobalUserPassword123!''
+    USING ''(DESCRIPTION =
+                (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+                (CONNECT_DATA = (SERVICE_NAME = ESHOP_GLOBAL)))''';
+/
+
+-- Test LINK_TO_GLOBAL from ESHOP_MUNTENIA
+BEGIN
+    FOR rec IN (SELECT 'LINK_TO_GLOBAL from ESHOP_MUNTENIA is working' AS status FROM dual@link_to_global) LOOP
+        DBMS_OUTPUT.PUT_LINE(rec.status);
+    END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error testing LINK_TO_GLOBAL from ESHOP_MUNTENIA: ' || SQLERRM);
+END;
+/
 
 -- Switch to ESHOP_ROMANIA PDB
 ALTER SESSION SET CONTAINER = eshop_romania;
 
-DECLARE
-    link_exists NUMBER := 0;
-BEGIN
-    SELECT COUNT(*) INTO link_exists
-    FROM ALL_DB_LINKS
-    WHERE DB_LINK = 'LINK_TO_GLOBAL';
+-- Set the schema to the desired user
+ALTER SESSION SET CURRENT_SCHEMA = ESHOP_ROMANIA_USER;
 
-    IF link_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE DATABASE LINK link_to_global
-            CONNECT TO "global_admin" IDENTIFIED BY "GlobalAdminPassword123!"
-            USING ''(DESCRIPTION =
-                (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
-                (CONNECT_DATA = (SERVICE_NAME = ESHOP_GLOBAL)))''';
-    END IF;
+-- Drop and recreate LINK_TO_GLOBAL
+BEGIN
+    EXECUTE IMMEDIATE 'DROP DATABASE LINK link_to_global';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN -- Ignore "link does not exist" error
+            RAISE;
+        END IF;
 END;
 /
+
+EXECUTE IMMEDIATE '
+    CREATE DATABASE LINK link_to_global
+    CONNECT TO eshop_global_user IDENTIFIED BY ''GlobalUserPassword123!''
+    USING ''(DESCRIPTION =
+                (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+                (CONNECT_DATA = (SERVICE_NAME = ESHOP_GLOBAL)))''';
+/
+
+-- Test LINK_TO_GLOBAL from ESHOP_ROMANIA
+BEGIN
+    FOR rec IN (SELECT 'LINK_TO_GLOBAL from ESHOP_ROMANIA is working' AS status FROM dual@link_to_global) LOOP
+        DBMS_OUTPUT.PUT_LINE(rec.status);
+    END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error testing LINK_TO_GLOBAL from ESHOP_ROMANIA: ' || SQLERRM);
+END;
