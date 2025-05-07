5ceaa1cb935a9d31a025a96e970bb515357516be
DESKTOP-MNEIOPV\Stefan <grigorescu2001@gmail.com>
2025-05-07 20:53:20 +0300

    (Not tested yet) Changed: Moved all sales tables in local databases Changed: Seed all tables from oltp db instead of copy pasting the seed code. This will only be ran once at table creation.

--- OracleDocker/mounts/scripts/startup/051_global_create_tables.sql
+++ OracleDocker/mounts/scripts/startup/051_global_create_tables.sql
@@ -30,11 +30,17 @@ BEGIN
     WHERE UPPER(TABLE_NAME) = UPPER(tbl_name);
 
     IF table_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE TABLE ' || UPPER(tbl_name) || ' ( ' ||
-            cols_and_constraints_csv || ' 
-        )';
-        LOG_INFORMATION('TRY_CREATE_TABLE: Table ' || tbl_name || ' created.', created_by);
+        BEGIN
+            EXECUTE IMMEDIATE '
+                CREATE TABLE ' || UPPER(tbl_name) || ' ( ' ||
+                cols_and_constraints_csv || ' 
+            )';
+            LOG_INFORMATION('TRY_CREATE_TABLE: Table ' || tbl_name || ' created.', created_by);
+        EXCEPTION
+            WHEN OTHERS THEN
+                LOG_ERROR('TRY_CREATE_TABLE: Error creating table ' || tbl_name || ': ' || SQLERRM, created_by);
+                RAISE_APPLICATION_ERROR(-20001, 'Error creating table ' || tbl_name || ': ' || SQLERRM);
+        END;
     ELSE
         LOG_DEBUG('TRY_CREATE_TABLE: Table ' || tbl_name || ' already exists.', created_by);
     END IF;
@@ -48,6 +54,7 @@ END;
 CREATE OR REPLACE PROCEDURE TRY_CREATE_SHARD (
     shard_name IN VARCHAR2,
     shard_query IN VARCHAR2,
+    alter_commands IN VARCHAR2 DEFAULT '',
     created_by IN VARCHAR2 DEFAULT NULL
 ) IS
     shard_exists NUMBER := 0;
@@ -70,10 +77,21 @@ BEGIN
     WHERE UPPER(TABLE_NAME) = UPPER(shard_name);
 
     IF shard_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-            CREATE TABLE ' || UPPER(shard_name) || ' AS ' || shard_query
-        ;
-        LOG_INFORMATION('TRY_CREATE_SHARD: Shard ' || shard_name || ' created.', created_by);
+        BEGIN
+            EXECUTE IMMEDIATE '
+                CREATE TABLE ' || UPPER(shard_name) || ' AS ' || shard_query
+            ;
+            LOG_INFORMATION('TRY_CREATE_SHARD: Shard ' || shard_name || ' created.', created_by);
+
+            IF alter_commands IS NOT NULL AND alter_commands <> '' THEN
+                EXECUTE IMMEDIATE alter_commands;
+                LOG_INFORMATION('TRY_CREATE_SHARD: Alter commands executed for ' || shard_name || '.', created_by);
+            END IF;
+        EXCEPTION
+            WHEN OTHERS THEN
+                LOG_ERROR('TRY_CREATE_SHARD: Error creating shard ' || shard_name || ': ' || SQLERRM, created_by);
+                RAISE_APPLICATION_ERROR(-20001, 'Error creating shard ' || shard_name || ': ' || SQLERRM);
+        END;
     ELSE
         LOG_DEBUG('TRY_CREATE_SHARD: Shard ' || shard_name || ' already exists.', created_by);
     END IF;
@@ -85,43 +103,25 @@ END;
 
 
 BEGIN
-    TRY_CREATE_TABLE('IDNT_REGIONS', ' 
-        id NUMBER PRIMARY KEY,
-        name NVARCHAR2(150) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_REGIONS',
+        'SELECT *
+        FROM IDNT_REGIONS@eshop_oltp_link',
+        '', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_CITIES', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        region_id NUMBER NOT NULL,
-        name NVARCHAR2(150) NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_idnt_cities_idnt_regions FOREIGN KEY (region_id) REFERENCES IDNT_Regions(id)
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_CITIES',
+        'SELECT *
+        FROM IDNT_CITIES@eshop_oltp_link',
+        '
+        ALTER TABLE IDNT_CITIES ADD CONSTRAINT fk_idnt_cities_idnt_regions FOREIGN KEY (region_id) REFERENCES IDNT_Regions(id);
+        ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_ROLES', ' 
-        id NUMBER PRIMARY KEY,
-        name VARCHAR2(25) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-
-    -- TRY_CREATE_TABLE('IDNT_USERS', ' 
-    --     id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    --     username NVARCHAR2(25) UNIQUE NOT NULL,
-    --     first_name NVARCHAR2(150) NOT NULL,
-    --     last_name NVARCHAR2(150) NOT NULL,
-    --     date_of_birth DATE NOT NULL,
-    --     email VARCHAR2(320) UNIQUE NOT NULL,
-    --     phone_number VARCHAR2(25),
-    --     password VARCHAR2(255) NOT NULL,
-    --     salt VARCHAR2(255) NOT NULL,
-    --     region_id NUMBER DEFAULT NULL,
-    --     created_on DATE DEFAULT SYSDATE NOT NULL,
-    --     last_updated_on DATE DEFAULT NULL
-    -- ', 'oltp_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_ROLES',
+        'SELECT *
+        FROM IDNT_ROLES@eshop_oltp_link',
+        '', 'global_create_tables');
     
     TRY_CREATE_SHARD(
         'IDNT_USERS',
@@ -133,212 +133,55 @@ BEGIN
         FROM IDNT_USERS@eshop_oltp_link
         ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_USER_ROLES', ' 
-        user_id NUMBER,
-        role_id NUMBER,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        CONSTRAINT fk_idnt_user_roles_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id),
-        CONSTRAINT fk_idnt_user_roles_idnt_roles FOREIGN KEY (role_id) REFERENCES IDNT_Roles(id),
-        PRIMARY KEY (user_id, role_id)
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_USER_ROLES',
+        'SELECT *
+        FROM IDNT_USER_ROLES@eshop_oltp_link',
+        '
+        ALTER TABLE IDNT_USER_ROLES ADD CONSTRAINT fk_idnt_user_roles_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id);
+        ALTER TABLE IDNT_USER_ROLES ADD CONSTRAINT fk_idnt_user_roles_idnt_roles FOREIGN KEY (role_id) REFERENCES IDNT_Roles(id);
+        ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_USER_ADDRESSES', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        user_id NUMBER NOT NULL,
-        city_id NUMBER NOT NULL,
-        street NVARCHAR2(250) DEFAULT NULL,
-        str_number NUMBER DEFAULT NULL,
-        postal_code VARCHAR2(25) DEFAULT NULL,
-        other_details NVARCHAR2(250) DEFAULT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_idnt_user_addresses_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id),
-        CONSTRAINT fk_idnt_user_addresses_idnt_cities FOREIGN KEY (city_id) REFERENCES IDNT_Cities(id)
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_USER_ADDRESSES',
+        'SELECT *
+        FROM IDNT_USER_ADDRESSES@eshop_oltp_link',
+        '
+        ALTER TABLE IDNT_USER_ADDRESSES ADD CONSTRAINT fk_idnt_user_addresses_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id);
+        ALTER TABLE IDNT_USER_ADDRESSES ADD CONSTRAINT fk_idnt_user_addresses_idnt_cities FOREIGN KEY (city_id) REFERENCES IDNT_Cities(id);
+        ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_INVITATION_LINKS_WITH_ROLE', ' 
-        value VARCHAR2(150) PRIMARY KEY,
-        sender_id NUMBER NOT NULL,
-        role_id NUMBER DEFAULT NULL,
-        expires_on DATE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_idnt_invitation_links_with_role_idnt_users FOREIGN KEY (sender_id) REFERENCES IDNT_Users(id),
-        CONSTRAINT fk_idnt_invitation_links_with_role_idnt_roles FOREIGN KEY (role_id) REFERENCES IDNT_Roles(id)
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_INVITATION_LINKS_WITH_ROLE',
+        'SELECT *
+        FROM IDNT_INVITATION_LINKS_WITH_ROLE@eshop_oltp_link',
+        '
+        ALTER TABLE IDNT_INVITATION_LINKS_WITH_ROLE ADD CONSTRAINT fk_idnt_invitation_links_with_role_idnt_users FOREIGN KEY (sender_id) REFERENCES IDNT_Users(id);
+        ALTER TABLE IDNT_INVITATION_LINKS_WITH_ROLE ADD CONSTRAINT fk_idnt_invitation_links_with_role_idnt_roles FOREIGN KEY (role_id) REFERENCES IDNT_Roles(id);
+        ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('IDNT_NOTIFICATIONS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        user_id NUMBER NOT NULL, 
-        message NVARCHAR2(850) NOT NULL,
-        metadata CLOB,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_idnt_notifications_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id)
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'IDNT_NOTIFICATIONS',
+        'SELECT *
+        FROM IDNT_NOTIFICATIONS@eshop_oltp_link',
+        '
+        ALTER TABLE IDNT_NOTIFICATIONS ADD CONSTRAINT fk_idnt_notifications_idnt_users FOREIGN KEY (user_id) REFERENCES IDNT_Users(id);
+        ', 'global_create_tables');
 
-    TRY_CREATE_TABLE('PCHS_VENDORS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        name NVARCHAR2(150) UNIQUE NOT NULL,
-        contact_info NVARCHAR2(850) DEFAULT NULL,
-        notes NVARCHAR2(850) DEFAULT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-    
-    TRY_CREATE_TABLE('PCHS_PRODUCTS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        vendor_id NUMBER NOT NULL,
-        price_in_eur NUMBER(10, 2) NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_pchs_products_pchs_vendors FOREIGN KEY (vendor_id) REFERENCES PCHS_Vendors(id), 
-        CONSTRAINT chk_pchs_products_product_price_in_eur_positive 
-            CHECK (price_in_eur >= 0) 
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'PCHS_VENDORS',
+        'SELECT *
+        FROM PCHS_VENDORS@eshop_oltp_link',
+        '', 'global_create_tables');
 
-    TRY_CREATE_TABLE('SLS_PRODUCTS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        name NVARCHAR2(150) NOT NULL,
-        description NVARCHAR2(850) DEFAULT NULL,
-        price_in_eur NUMBER(10, 2) NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL, 
-        CONSTRAINT chk_sls_products_product_price_in_eur_positive 
-            CHECK (price_in_eur >= 0) 
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_PRODUCT_CATEGORIES', ' 
-        id NUMBER PRIMARY KEY,
-        name NVARCHAR2(150) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_PRODUCT_SUBCATEGORIES', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        category_id NUMBER NOT NULL,
-        name NVARCHAR2(150) NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_sls_product_subcategories_sls_product_categories FOREIGN KEY (category_id) REFERENCES SLS_Product_Categories(id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_PRODUCT_PRODUCT_SUBCATEGORIES', ' 
-        product_id NUMBER,
-        subcategory_id NUMBER,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        CONSTRAINT fk_sls_product_product_subcategories_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id),
-        CONSTRAINT fk_sls_product_product_subcategories_sls_product_subcategories FOREIGN KEY (subcategory_id) REFERENCES SLS_Product_Subcategories(id),
-        PRIMARY KEY (product_id, subcategory_id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_PRODUCT_TAGS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        name NVARCHAR2(25) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_PRODUCT_PRODUCT_TAGS', ' 
-        product_id NUMBER,
-        tag_id NUMBER,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        CONSTRAINT fk_sls_product_product_tags_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id),
-        CONSTRAINT fk_sls_product_product_tags_sls_product_tags FOREIGN KEY (tag_id) REFERENCES SLS_Product_Tags(id),
-        PRIMARY KEY (product_id, tag_id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_DISCOUNT_TYPES', ' 
-        id NUMBER PRIMARY KEY,
-        name VARCHAR2(25) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_DISCOUNT_REASONS', ' 
-        id NUMBER PRIMARY KEY,
-        name VARCHAR2(25) UNIQUE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_DISCOUNTS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        product_id NUMBER NOT NULL,
-        discount_type_id NUMBER NOT NULL,
-        name VARCHAR2(25) NOT NULL,
-        description NVARCHAR2(850) DEFAULT NULL,
-        amount NUMBER(10, 2) NOT NULL,
-        starts_on DATE NOT NULL,
-        ends_on DATE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_sls_discounts_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id),
-        CONSTRAINT fk_sls_discounts_sls_discount_types FOREIGN KEY (discount_type_id) REFERENCES SLS_Discount_Types(id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_BUNDLE_DISCOUNTS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        discount_type_id NUMBER NOT NULL,
-        name VARCHAR2(25) NOT NULL,
-        description NVARCHAR2(850) DEFAULT NULL,
-        amount NUMBER(10, 2) NOT NULL,
-        starts_on DATE NOT NULL,
-        ends_on DATE NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_sls_bundle_discounts_sls_discount_types FOREIGN KEY (discount_type_id) REFERENCES SLS_Discount_Types(id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_BUNDLE_DISCOUNT_PRODUCTS', ' 
-        bundle_id NUMBER,
-        product_id NUMBER,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        CONSTRAINT fk_sls_bundle_discount_products_sls_bundle_discounts FOREIGN KEY (bundle_id) REFERENCES SLS_Bundle_Discounts(id),
-        CONSTRAINT fk_sls_bundle_discount_products_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id),
-        PRIMARY KEY (bundle_id, product_id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_MY_WISHLIST_ITEMS', ' 
-        customer_id NUMBER,
-        product_id NUMBER,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        CONSTRAINT fk_sls_my_wishlist_items_idnt_users FOREIGN KEY (customer_id) REFERENCES IDNT_Users(id),
-        CONSTRAINT fk_sls_my_wishlist_items_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id),
-        PRIMARY KEY (customer_id, product_id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_MY_SHOPPING_CART_ITEMS', ' 
-        customer_id NUMBER,
-        product_id NUMBER,
-        quantity NUMBER NOT NULL,
-        created_on DATE DEFAULT SYSDATE NOT NULL,
-        last_updated_on DATE DEFAULT NULL,
-        CONSTRAINT fk_sls_my_shopping_cart_items_idnt_users FOREIGN KEY (customer_id) REFERENCES IDNT_Users(id),
-        CONSTRAINT fk_sls_my_shopping_cart_items_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id), 
-        CONSTRAINT chk_sls_my_shopping_cart_items_quantity_positive 
-            CHECK (quantity >= 0), 
-        PRIMARY KEY (customer_id, product_id)
-    ', 'global_create_tables');
-
-    TRY_CREATE_TABLE('SLS_MY_DISCOUNT_COUPONS', ' 
-        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-        code VARCHAR2(25) NOT NULL, 
-        product_id NUMBER NOT NULL, 
-        discount_type_id NUMBER NOT NULL, 
-        order_id NUMBER DEFAULT NULL, 
-        amount NUMBER(10, 2) NOT NULL, 
-        starts_on DATE NOT NULL, 
-        ends_on DATE NOT NULL, 
-        discount_reason_id NUMBER NOT NULL, 
-        created_on DATE DEFAULT SYSDATE NOT NULL, 
-        last_updated_on DATE DEFAULT NULL, 
-        CONSTRAINT fk_sls_my_discount_coupons_sls_products FOREIGN KEY (product_id) REFERENCES SLS_Products(id), 
-        CONSTRAINT fk_sls_my_discount_coupons_sls_discount_types FOREIGN KEY (discount_type_id) REFERENCES SLS_Discount_Types(id), 
-        -- CONSTRAINT fk_sls_my_discount_coupons_sls_orders FOREIGN KEY (order_id) REFERENCES SLS_Orders(id), 
-        CONSTRAINT fk_sls_my_discount_coupons_sls_discount_reasons FOREIGN KEY (discount_reason_id) REFERENCES SLS_Discount_Reasons(id) 
-    ', 'global_create_tables');
+    TRY_CREATE_SHARD(
+        'PCHS_PRODUCTS',
+        'SELECT *
+        FROM PCHS_PRODUCTS@eshop_oltp_link',
+        '
+        ALTER TABLE PCHS_PRODUCTS ADD CONSTRAINT fk_pchs_products_pchs_vendors FOREIGN KEY (vendor_id) REFERENCES PCHS_Vendors(id); 
+        ALTER TABLE PCHS_PRODUCTS ADD CONSTRAINT chk_pchs_products_product_price_in_eur_positive CHECK (price_in_eur >= 0);
+        ', 'global_create_tables');
     
     LOG_INFORMATION('Finished creating tables.', 'global_create_tables');
 END;
--- OracleDocker/mounts/scripts/startup/063_romania_seed_data.sql
+++ OracleDocker/mounts/scripts/startup/063_romania_seed_data.sql
@@ -1,90 +1,76 @@
-SET SERVEROUTPUT ON;
+-- SET SERVEROUTPUT ON;
 
--- Switch to ROMANIA PDB
-ALTER SESSION SET CONTAINER = eshop_romania;
+-- -- Switch to ROMANIA PDB
+-- ALTER SESSION SET CONTAINER = eshop_romania;
 
--- Set the schema to the desired user
-ALTER SESSION SET CURRENT_SCHEMA = ESHOP_ROMANIA_USER;
+-- -- Set the schema to the desired user
+-- ALTER SESSION SET CURRENT_SCHEMA = ESHOP_ROMANIA_USER;
 
--- Create procedure to insert an order status
-CREATE OR REPLACE PROCEDURE INSERT_ORDER_STATUS (
-    p_status_id IN NUMBER,
-    p_status_name IN VARCHAR2
-) IS
-    status_exists_by_id NUMBER := 0;
-    status_exists_by_name NUMBER := 0;
-BEGIN
-    -- Check if the order status ID or name already exists
-    SELECT 
-        COUNT(CASE WHEN id = p_status_id THEN 1 END), COUNT(CASE WHEN UPPER(name) = UPPER(p_status_name) THEN 1 END) 
-        INTO status_exists_by_id, status_exists_by_name
-    FROM SLS_ORDER_STATUSES;
+-- -- Create procedure to insert an order status
+-- CREATE OR REPLACE PROCEDURE INSERT_ORDER_STATUS (
+--     p_status_id IN NUMBER,
+--     p_status_name IN VARCHAR2
+-- ) IS
+--     status_exists_by_id NUMBER := 0;
+--     status_exists_by_name NUMBER := 0;
+-- BEGIN
+--     -- Check if the order status ID or name already exists
+--     SELECT 
+--         COUNT(CASE WHEN id = p_status_id THEN 1 END), COUNT(CASE WHEN UPPER(name) = UPPER(p_status_name) THEN 1 END) 
+--         INTO status_exists_by_id, status_exists_by_name
+--     FROM SLS_ORDER_STATUSES;
 
-    -- Ensure both ID and name are unique
-    IF status_exists_by_id > 0 THEN
-        LOG_DEBUG('INSERT_ORDER_STATUS: Status ID ' || p_status_id || ' already exists.');
-    ELSIF status_exists_by_name > 0 THEN
-        LOG_DEBUG('INSERT_ORDER_STATUS: Status name ' || p_status_name || ' already exists.');
-    ELSE
-        -- Insert the order status if both ID and name are unique
-        INSERT INTO SLS_ORDER_STATUSES (id, name)
-        VALUES (p_status_id, p_status_name);
-        LOG_INFORMATION('INSERT_ORDER_STATUS: Status ' || p_status_name || ' with ID ' || p_status_id || ' created.');
-    END IF;
+--     -- Ensure both ID and name are unique
+--     IF status_exists_by_id > 0 THEN
+--         LOG_DEBUG('INSERT_ORDER_STATUS: Status ID ' || p_status_id || ' already exists.');
+--     ELSIF status_exists_by_name > 0 THEN
+--         LOG_DEBUG('INSERT_ORDER_STATUS: Status name ' || p_status_name || ' already exists.');
+--     ELSE
+--         -- Insert the order status if both ID and name are unique
+--         INSERT INTO SLS_ORDER_STATUSES (id, name)
+--         VALUES (p_status_id, p_status_name);
+--         LOG_INFORMATION('INSERT_ORDER_STATUS: Status ' || p_status_name || ' with ID ' || p_status_id || ' created.');
+--     END IF;
 
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        LOG_ERROR('INSERT_ORDER_STATUS: Error creating status ' || p_status_name || ': ' || SQLERRM);
-        ROLLBACK;
-END;
-/
+--     COMMIT;
+-- EXCEPTION
+--     WHEN OTHERS THEN
+--         LOG_ERROR('INSERT_ORDER_STATUS: Error creating status ' || p_status_name || ': ' || SQLERRM);
+--         ROLLBACK;
+-- END;
+-- /
 
-BEGIN
-    INSERT_ORDER_STATUS(0, 'Pending');
-    INSERT_ORDER_STATUS(1, 'Completed');
-    INSERT_ORDER_STATUS(2, 'Canceled');
-END;
-/
 
--- Create procedure to insert an invoice status
-CREATE OR REPLACE PROCEDURE INSERT_INVOICE_STATUS (
-    p_status_id IN NUMBER,
-    p_status_name IN VARCHAR2
-) IS
-    status_exists_by_id NUMBER := 0;
-    status_exists_by_name NUMBER := 0;
-BEGIN
-    -- Check if the invoice status ID or name already exists
-    SELECT 
-        COUNT(CASE WHEN id = p_status_id THEN 1 END), COUNT(CASE WHEN UPPER(name) = UPPER(p_status_name) THEN 1 END) 
-        INTO status_exists_by_id, status_exists_by_name
-    FROM BLG_INVOICE_STATUSES;
+-- -- Create procedure to insert an invoice status
+-- CREATE OR REPLACE PROCEDURE INSERT_INVOICE_STATUS (
+--     p_status_id IN NUMBER,
+--     p_status_name IN VARCHAR2
+-- ) IS
+--     status_exists_by_id NUMBER := 0;
+--     status_exists_by_name NUMBER := 0;
+-- BEGIN
+--     -- Check if the invoice status ID or name already exists
+--     SELECT 
+--         COUNT(CASE WHEN id = p_status_id THEN 1 END), COUNT(CASE WHEN UPPER(name) = UPPER(p_status_name) THEN 1 END) 
+--         INTO status_exists_by_id, status_exists_by_name
+--     FROM BLG_INVOICE_STATUSES;
 
-    -- Ensure both ID and name are unique
-    IF status_exists_by_id > 0 THEN
-        LOG_DEBUG('INSERT_INVOICE_STATUS: Status ID ' || p_status_id || ' already exists.');
-    ELSIF status_exists_by_name > 0 THEN
-        LOG_DEBUG('INSERT_INVOICE_STATUS: Status name ' || p_status_name || ' already exists.');
-    ELSE
-        -- Insert the invoice status if both ID and name are unique
-        INSERT INTO BLG_INVOICE_STATUSES (id, name)
-        VALUES (p_status_id, p_status_name);
-        LOG_INFORMATION('INSERT_INVOICE_STATUS: Status ' || p_status_name || ' with ID ' || p_status_id || ' created.');
-    END IF;
+--     -- Ensure both ID and name are unique
+--     IF status_exists_by_id > 0 THEN
+--         LOG_DEBUG('INSERT_INVOICE_STATUS: Status ID ' || p_status_id || ' already exists.');
+--     ELSIF status_exists_by_name > 0 THEN
+--         LOG_DEBUG('INSERT_INVOICE_STATUS: Status name ' || p_status_name || ' already exists.');
+--     ELSE
+--         -- Insert the invoice status if both ID and name are unique
+--         INSERT INTO BLG_INVOICE_STATUSES (id, name)
+--         VALUES (p_status_id, p_status_name);
+--         LOG_INFORMATION('INSERT_INVOICE_STATUS: Status ' || p_status_name || ' with ID ' || p_status_id || ' created.');
+--     END IF;
 
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        LOG_ERROR('INSERT_INVOICE_STATUS: Error creating status ' || p_status_name || ': ' || SQLERRM);
-        ROLLBACK;
-END;
-/
-
-BEGIN
-    INSERT_INVOICE_STATUS(0, 'Pending');
-    INSERT_INVOICE_STATUS(1, 'Paid');
-    INSERT_INVOICE_STATUS(2, 'Overdue');
-    INSERT_INVOICE_STATUS(3, 'Canceled');
-END;
-/
+--     COMMIT;
+-- EXCEPTION
+--     WHEN OTHERS THEN
+--         LOG_ERROR('INSERT_INVOICE_STATUS: Error creating status ' || p_status_name || ': ' || SQLERRM);
+--         ROLLBACK;
+-- END;
+-- /
